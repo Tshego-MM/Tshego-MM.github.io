9a731ea489f8cc98aa4f90478d3e1c72906e2b0a
Tshego-MM <132884419+Tshego-MM@users.noreply.github.com>
2023-05-10 10:31:26 +0200

    Added more functionality

--- calculator.js
+++ calculator.js
@@ -7,7 +7,7 @@ function calculate(operand1, operand2, operator){
         case "-":
             results = operand1-operand2;
             break;
-        case "/":
+        case "÷":
             if(operand2===0){
                 throw new Error("Division by zero is not allowed");  
             }
@@ -15,10 +15,10 @@ function calculate(operand1, operand2, operator){
                 results = operand1/operand2;
             }
             break;
-        case "*":
+        case "×":
             results = operand1*operand2;
             break;
-        case 'e':
+        case '^':
             if(operand1===0 && operand2===0){
                 throw new Error("zero power zero is not allowed");
             }
@@ -50,6 +50,17 @@ function sci(operand, operator){
                 results = Math.tan(operand);
             }
             break;
+        case '√':
+            if(operand<0){
+                throw new Error("Cannot find square root of negative numbers");
+            }
+            else{
+                results = Math.sqrt(operand);
+            }
+            break;
+        case '%':
+            results = (operand/100);
+            break;
     }
     
     return results;
@@ -61,16 +72,16 @@ function precValue(operator){
      */
     let value = 0;
     switch(operator){
-        case '+':
-            value = 4;
-            break;
         case '-':
             value = 5;
             break;
-        case '*':
+        case '+':
+            value = 4;
+            break;
+        case '×':
             value = 3;
             break;
-        case '/':
+        case '÷':
             value = 2;
             break;
         case '(':
@@ -79,7 +90,7 @@ function precValue(operator){
         case ')':
             value = 0;
             break;
-        case 'e':
+        case '^':
             value = 1;
             break;
         default:
@@ -106,13 +117,13 @@ function infixPos(infix){
     let stack = [];  //keeps track of our brackets
     let postfix = []; //stores the resulting postfix notation
     for(let i=0; i<infix.length; ++i){
-        if(+infix[i]){
+        if(infix[i]==='0' || +infix[i]){
             postfix.push(infix[i]);
         }
         else{
             if(infix[i]===')'){
                 let lastVal = stack.pop(); //The last element of the array
-                while(lastVal!=='('){
+                while(lastVal && lastVal!=='('){
                     postfix.push(lastVal);
                     lastVal = stack.pop();
                 }
@@ -128,33 +139,92 @@ function infixPos(infix){
                     stack.push(infix[i]);
                 }
                 else{
-                    while(stack.length>0 && !precedence(infix[i], stack[stack.length-1])){
-                        postfix.push(stack.pop());
+                    let stackTop = stack.pop(); //The last element of the array
+                    while(stackTop && !precedence(infix[i], stack[stack.length-1]) && stackTop!=='('){
+                        postfix.push(stackTop);
+                        stackTop = stack.pop();
                     }
+                    stack.push(infix[i]);
                 }
             }
-
         }
     }
-
-    while(stack.length!==0){
+    
+    while(stack.length>0){
         postfix.push(stack.pop())
     }
     
     return postfix;
 }
 
-function results(infix){
+function preprocess(inputStr){
+    /**
+     * Takes the input string and preprocesses it
+     * And returns an array of the numbers and operations
+     * This is to make the string compatible with our calculation functions
+     */
+    let inputArr = []; //The operands and operators
+    let tmpStr = ""; //Temporary string to build numbers with more than one digit
+    inputStr = inputStr.replace("sin", "s");
+    inputStr = inputStr.replace("cos", "c");
+    inputStr = inputStr.replace("tan", "t");
+    const NoMultPattern = /[)\d]{1}[sct√]/g;  //This will find number implicitly multiplied by trig function (e.g 2sin(1)) and adds the multiplication
+    const toFix = inputStr.match(NoMultPattern); //contains the values to fix
+    if(toFix){
+        toFix.forEach(element => {
+            inputStr = inputStr.replaceAll(element, element.charAt(0)+'×'+element.charAt(1));
+        });
+    }
+
+    for(let i=0; i<inputStr.length; ++i){
+        if(inputStr.charAt(i)==='0' || +inputStr.charAt(i) || inputStr.charAt(i)==='.'){
+            tmpStr += inputStr.charAt(i);
+        }
+        else{
+            if(tmpStr.length>0){
+                inputArr.push(tmpStr);
+                tmpStr = "";
+            }
+            else if(inputStr.charAt(i)==='-'){
+                if(i===0){
+                    inputArr.push('-1');
+                    inputArr.push('×');
+                    continue;
+                }
+                else if(['+', '-', '÷', '×', '('].includes(inputStr.charAt(i-1))){
+                    inputArr.push('-1');
+                    inputArr.push('×');
+                    continue;
+                }
+            }
+            else if(inputStr.charAt(i)==='+'){
+                if(i===0 || ['+', '-', '÷', '×', '('].includes(inputStr.charAt(i-1))){
+                    continue;
+                }
+            }
+            inputArr.push(inputStr.charAt(i));
+        }
+    }
+    if(tmpStr.length>0){   //The last number from the string
+        inputArr.push(tmpStr);
+        tmpStr = "";
+    }
+
+    return inputArr;
+}
+
+function compute(inputStr){
+    const infix = preprocess(inputStr);
     const postfix = infixPos(infix);
     let stack = [];
     for(let i=0; i<postfix.length; ++i){
-        if(+postfix[i]){
+        if(postfix[i]==='0' || +postfix[i]){
             stack.push(+postfix[i]);
         }
         else{
             try{
                 let res = 0, v1 = stack.pop();
-                if(postfix[i].includes('s') || postfix[i].includes('c') || postfix[i].includes('t')){
+                if(['√', '%', 's', 'c', 't'].includes(postfix[i])){
                     res = sci(v1, postfix[i]);
                 }
                 else{
@@ -164,10 +234,12 @@ function results(infix){
                 stack.push(res);
             }
             catch(err){
-                console.log(err.message);
+                alert(err.message);
             }
         }
     }
     
     return stack.pop();
 }
+
+export default compute;
